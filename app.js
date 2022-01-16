77c183e0c380ce1c8693e73ad145a3192a16e2f5
navidabdi <artsanaat@gmail.com>
2022-01-16 10:00:54 +0330

    Select The Controllers

---
+++ app.js
@@ -0,0 +1,17 @@
+// Select The Number Buttons
+const numBtns = document.querySelectorAll("[data-number]");
+// Select The Operation Buttons
+const operationBtns = document.querySelectorAll("[data-operation]");
+// Select The Equals Button
+const equalsBtn = document.querySelector("[data-equals]");
+// Select The Delete Button
+const deleteBtn = document.querySelector("[data-delete]");
+// Select The All Clear Button
+const allClearBtn = document.querySelector("[data-all-clear]");
+// Select The Previous And Current Operands
+const previousOperandTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
+const currentOperandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
