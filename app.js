60d6367c51089911545c8678f39523139c5765c1
navidabdi <artsanaat@gmail.com>
2022-01-16 14:29:17 +0330

    Add JavaScript Functionality

--- app.js
+++ app.js
@@ -1,3 +1,87 @@
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
+  }
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+  }
+  appendNumber(number) {
+    if (number === "." && this.currentOperand.includes(".")) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+  }
+  chooseOperation(operation) {
+    if (this.currentOperand === "") return;
+    if (this.previousOperand !== "") {
+      this.compute();
+    }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
+  }
+  compute() {
+    let computation;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (this.operation) {
+      case "+":
+        computation = prev + current;
+        break;
+      case "-":
+        computation = prev - current;
+        break;
+      case "×":
+        computation = prev * current;
+        break;
+      case "÷":
+        computation = prev / current;
+        break;
+      default:
+        return;
+    }
+    this.currentOperand = computation;
+    this.operation = undefined;
+    this.previousOperand = "";
+  }
+  getDisplayNumber(number) {
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const desimalDigits = stringNumber.split(".")[1];
+
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+      integerDisplay = "";
+    } else {
+      integerDisplay = integerDigits.toLocaleString("en", {
+        maximumFractionDigits: 0,
+      });
+    }
+
+    if (desimalDigits != null) {
+      return `${integerDisplay}.${desimalDigits}`;
+    } else {
+      return integerDisplay;
+    }
+  }
+  updateDisplay() {
+    this.currentOperandTextElement.innerText = this.getDisplayNumber(
+      this.currentOperand
+    );
+    if (this.operation != null) {
+      this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operation}`;
+    } else {
+      this.previousOperandTextElement.innerText = "";
+    }
+  }
+}
+
 // Select The Number Buttons
 const numBtns = document.querySelectorAll("[data-number]");
 // Select The Operation Buttons
@@ -15,3 +99,33 @@ const previousOperandTextElement = document.querySelector(
 const currentOperandTextElement = document.querySelector(
   "[data-current-operand]"
 );
+
+const calculator = new Calculator(
+  previousOperandTextElement,
+  currentOperandTextElement
+);
+
+numBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    calculator.appendNumber(btn.innerText);
+    calculator.updateDisplay();
+  });
+});
+operationBtns.forEach((operation) => {
+  operation.addEventListener("click", () => {
+    calculator.chooseOperation(operation.innerText);
+    calculator.updateDisplay();
+  });
+});
+equalsBtn.addEventListener("click", (btn) => {
+  calculator.compute();
+  calculator.updateDisplay();
+});
+allClearBtn.addEventListener("click", (btn) => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
+deleteBtn.addEventListener("click", (btn) => {
+  calculator.delete();
+  calculator.updateDisplay();
+});
